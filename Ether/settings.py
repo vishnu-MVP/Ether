9d75fc7af6e765affe48f613cab24d71fdebc5c8
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2022-10-01 00:16:15 +0530

    Update settings.py

--- Ether/settings.py
+++ Ether/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'Ether.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')    ],
+        'DIRS': [path.join(BASE_DIR, 'templates')    ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
